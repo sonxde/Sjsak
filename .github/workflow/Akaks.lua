01f7a9f7736e09af37e01c9c490f3dabf5112fa1
sonxde <sonixeryteam@gmail.com>
2025-06-13 10:07:42 +0700

    Create Akaks.lua

---
+++ .github/workflow/Akaks.lua
@@ -0,0 +1,7 @@
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Priv KDV3Modded..")
+task.wait(3)
+
+
+loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Just-A-Baseplate-Working-Reanimation-39126"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sonxde/Sjsak/refs/heads/main/.github/workflow/Askd.txt"))()
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Loaded.")
